9bacb4a93f98cc624d76e6486af27adf54de3031
pilotty1964-tech <pilotty1964@gmail.com>
2025-10-02 13:49:04 +0300

    Update Main.java

--- src/main/java/clases/Main.java
+++ src/main/java/clases/Main.java
@@ -1,9 +1,15 @@
 package main.java.clases;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import main.java.clases.dao.CarsDAO;
+import main.java.clases.gui.CarsAppGUI;
+import main.java.clases.util.AppProperties;
+import main.java.clases.model.Cars;
+
+import javax.swing.*;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-
+        SwingUtilities.invokeLater(CarsAppGUI::new);
     }
 }
